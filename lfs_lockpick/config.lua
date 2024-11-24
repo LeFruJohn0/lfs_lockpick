f03bb822e8df723c274dec0f1d7583e97886fc56
LeFruJohn0 <87871872+LeFruJohn0@users.noreply.github.com>
2024-11-24 14:03:47 +0200

    Update config.lua

--- lfs_lockpick/config.lua
+++ lfs_lockpick/config.lua
@@ -1,8 +1,8 @@
-Config = {}
-
-Config.LockpickStrength = {
-    ['lockpick'] = 0.5,
-    ['lockpickmold'] = 0.25,
+Config = {
+    LockpickStrength = {
+        ['lockpick'] = 0.5,
+        ['lockpickmold'] = 0.25,
+    },
+    DefaultDifficulty = 2,
+    DefaultPinAmount = 4,
 }
-Config.DefaultDifficulty = 2
-Config.DefaultPinAmount = 4
